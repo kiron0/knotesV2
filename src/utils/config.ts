1ef00ceaf84198bb723e0b4c91d879b1f717dd17
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2024-01-12 12:27:39 +0600

    minor changes on ui

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:3000/api";
+// export const BASE_URL = "http://localhost:3000/api";
 
-// export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string;
 
 export const MONGO_URL = process.env.NEXT_PUBLIC_MONGO_URL as string;
 
